eea3860c46d1bc38cdf2cdaee4cd1cef25588074
omidsn <omid.snnnn@gmail.com>
2022-07-30 17:47:07 +0430

    New APIs Added

---
+++ Domain/Domain/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using Domain.Entities;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Domain.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        public IDbConnection Connection { get; }
+        DatabaseFacade Database { get; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Department { get; set; }
+
+        Task<int> SaveChangeAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ SuperHeroAPI/SuperHeroAPI/SuperHero.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuperHeroAPI
+{
+    public class SuperHero
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Name { get; set; } = String.Empty;
+        public string FirstName { get; set; } = String.Empty;
+        public string LastName { get; set; } = String.Empty;
+        public string Place { get; set; } = String.Empty;
+    }
+}
